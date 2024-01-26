8b8eba82648e3f0a916d911d5c29ff59628b1b57
Maxim_Larin <maks.larin@gmail.com>
2024-01-26 17:20:14 +0300

    Версия 2

---
+++ MMORPG_board_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostCreate, ReplyAdd, PostEdit, Replies
+
+urlpatterns = [
+   path('', PostsList.as_view(), name='posts'),
+   path('<int:pk>', PostDetail.as_view(), name='post'),
+   # path('create/', PostCreate.as_view(), name='postcreate'),
+   # path('<int:pk>/reply/add', ReplyAdd.as_view(), name='reply_add'),
+   # path('<int:pk>/edit', PostEdit.as_view(), name='post_edit'),
+   # path('replies/', Replies.as_view(), name='replies'),
+   # path('delete/<int:pk>', delete_reply, name='delete_reply'),
+   # path('<int:pk>/delete', delete_post, name='delete_post'),
+   # path('<int:pk>/allow', allow_reply, name='allow_reply'),
+   # path('send_mails/', news_send, name='send_mails'),
+]
--- MMORPG_board_app/views.py
+++ MMORPG_board_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Category, Post, Reply
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-# Create your views here.
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'posts_template.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_logged'] = self.request.user.is_authenticated
+        context['current_user'] = self.request.user
+        context['user_is_staff'] = self.request.user.is_staff
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail_template.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        replies_by_post_id = Reply.objects.filter(post=self.kwargs['pk']).order_by('-date_posted')
+        context['is_logged'] = self.request.user.is_authenticated
+        context['replys'] = replies_by_post_id
+        return context
